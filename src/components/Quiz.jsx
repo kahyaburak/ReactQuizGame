e3d340d3815809fe4481fabc2f242d78ef9d5e1d
Burak <burakkahyabk@gmail.com>
2024-06-14 23:46:05 +0300

    Added Question Timers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,38 +1,49 @@
-import {useState} from 'react';
+import {useState, useCallback} from 'react';
+
 import QUESTIONS from '../questions.js';
-import quizComplete from '../assets/quiz-complete.png';
+import QuestionTimer from './QuestionTimer.jsx';
+import quizCompleteImg from '../assets/quiz-complete.png';
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
-  const currentQuestion = QUESTIONS[activeQuestionIndex];
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-  function handleSelectAnswer(selectedAnswer) {
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(
+    selectedAnswer
+  ) {
     setUserAnswers((prevUserAnswers) => {
       return [...prevUserAnswers, selectedAnswer];
     });
-  }
+  },
+  []);
+
+  const handleSkipAnswer = useCallback(
+    () => handleSelectAnswer(null),
+    [handleSelectAnswer]
+  );
 
   if (quizIsComplete) {
     return (
       <div id="summary">
-        <img src={quizComplete} alt="Thropy icon" />
-        <h2>Quiz Completed</h2>
+        <img src={quizCompleteImg} alt="Trophy icon" />
+        <h2>Quiz Completed!</h2>
       </div>
     );
   }
-  const shuffledAnswers = [...currentQuestion.answers];
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
   shuffledAnswers.sort(() => Math.random() - 0.5);
 
   return (
     <div id="quiz">
       <div id="question">
-        <h2>{currentQuestion.text}</h2>
+        <QuestionTimer timeout={10000} onTimeout={handleSkipAnswer} />
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
           {shuffledAnswers.map((answer) => (
-            <li className="answer" key={answer}>
+            <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
               </button>
