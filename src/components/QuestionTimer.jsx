b82e8c7d447762f5326e16a08fbaf4d7c3a7c8ff
Burak <burakkahyabk@gmail.com>
2024-06-15 19:29:34 +0300

    Cleared timer and intervals, and added a key to the QuestionTimer component to recreate it after every re-render

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -4,15 +4,19 @@ export default function QuestionTimer({timeout, onTimeout}) {
   const [remainingTime, setRemainingTime] = useState(timeout);
 
   useEffect(() => {
-    console.log('SETTING TIMEOUT');
-    setTimeout(onTimeout, timeout);
+    const timer = setTimeout(onTimeout, timeout);
+    return () => {
+      clearTimeout(timer);
+    };
   }, [timeout, onTimeout]);
 
   useEffect(() => {
-    console.log('SETTING INTERVAL');
-    setInterval(() => {
+    const interval = setInterval(() => {
       setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
     }, 100);
+    return () => {
+      clearInterval(interval);
+    };
   }, []);
 
   return <progress id="question-time" max={timeout} value={remainingTime} />;
