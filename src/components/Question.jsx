91b1d884ac74140b43ab0590d7792fc07e203afb
Burak <burakkahyabk@gmail.com>
2024-06-16 21:14:03 +0300

    Added Summary Component

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -14,7 +14,7 @@ export default function Question({
     isCorrect: null,
   });
 
-  let timer = 10000;
+  let timer = 5000;
   if (answer.selectedAnswer) timer = 1000;
   if (answer.isCorrect !== null) timer = 2000;
 
